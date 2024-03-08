291db34ee23ccda3dffa012e2e7a8feb4228a7f2
Daria Tsvetkova <lorminel22@gmail.com>
2024-03-08 15:06:55 +0300

    Added getMessage() method

--- src/main/java/com/lorminel22/Main.java
+++ src/main/java/com/lorminel22/Main.java
@@ -3,6 +3,10 @@ package com.lorminel22;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello qa_gutu!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello qa_guru!";
     }
 }
